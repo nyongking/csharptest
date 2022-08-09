ec285abe34d2c899412c548948435c9223229e9c
nyongking <niyongking@gmail.com>
2022-08-09 21:34:52 +0900

    ver1

--- csharptest/Program.cs
+++ csharptest/Program.cs
@@ -6,7 +6,30 @@ namespace csharptest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CursorVisible = false; // not cursor viewable
+
+            int lastTick = 0;
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+            while (true)
+            {
+                #region frame
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+                #endregion
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
